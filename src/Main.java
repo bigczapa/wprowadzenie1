e680fdbded16868add49833cf9835f9129998cdf
bigczapa <czokapikdx2@gmail.com>
2025-09-15 12:42:53 +0200

    typy proste

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,32 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public static void main(String[] args){
+        System.out.println("Dzień dobry");
+
+        byte liczba1 =8;
+        short liczba2 =300;
+        int liczba3 = 12345678;
+        long liczba4 = 871364082;
+        System.out.println("Twoja liczba " + liczba1);
+
+
+        //typy zmiennoprzecinkowe
+        float LiczbaRzeczywista = 3.14f;
+        double liczbaRzeczywista2 = 23.45678;
+
+        System.out.println("Liczba rzeczywista " + LiczbaRzeczywista);
+
+
+        //typ logiczny
+        boolean czyPrawda = true;
+
+
+        //typ znakowy
+        char znak = 'a';
+
+        //napisz program w ktorym wylosujesz liczbe od 1 do 100, wpiszesz liczbe z klawiatury, powiesz czy ktos zgadl porządaną
+
     }
 }
